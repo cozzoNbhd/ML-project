90105ce9c3880388e0a789964bcf274cd8740185
Gabibbocomrade <Tank2001@hotmail.it>
2024-12-20 18:42:56 +0100

    Sistemato file

--- NN.py
+++ NN.py
@@ -1,11 +1,16 @@
 import numpy
+import numpy as np
 from keras import Sequential
-from keras.callbacks import EarlyStopping
-from keras.layers import Dense
+from keras.callbacks import EarlyStopping,LearningRateScheduler
+from keras.layers import Dense, BatchNormalization, CategoryEncoding, Flatten,Input
+from tensorflow.keras.models import Model
 from keras.losses import BinaryCrossentropy
-from keras.optimizers import SGD
-from sklearn.model_selection import train_test_split
-
+from keras.optimizers import SGD, RMSprop, Adam
+from sklearn.model_selection import train_test_split, KFold
+import matplotlib.pyplot as plt
+from keras import regularizers
+from sklearn.preprocessing import OneHotEncoder
+import itertools
 from utilities import DatasetProcessor  # Importa la classe dal file utilities.py
 
 
@@ -17,7 +22,7 @@ def train_neural_network(train_path, test_path):
     df_train, df_test = processor.load_dataset(train_path, test_path)
 
     X_train_full, y_train_full, X_test, y_test = processor.preprocess_data(df_train, df_test)
-    X_train_fscaled, X_test_fscaled = processor.normalize_data(X_train_full, X_test)
+
     # Dividi i dati di training in train e validation
     X_train, X_val, y_train, y_val = train_test_split(X_train_full, y_train_full, test_size=0.3, random_state=42)
 
@@ -39,14 +44,16 @@ def train_neural_network(train_path, test_path):
     print(X_train_encoded.shape)
     # Creazione del modello
     model = Sequential([
-        Dense(64, activation='relu', input_shape=(X_train.shape[1],)),  # Primo strato nascosto
-        Dense(32, activation='relu'),  # Secondo strato nascosto
+        Dense(64, activation='relu', input_shape=(X_train_encoded.shape[1],),  kernel_regularizer=regularizers.L2(0.01)),  # Primo strato nascosto
+
+        Dense(32, activation='relu', kernel_regularizer=regularizers.L2(0.01)),  # Secondo strato nascosto
+
         Dense(1, activation='sigmoid')  # Strato di output con sigmoid
     ])
 
     # Compilazione del modello
     model.compile(
-        optimizer=SGD(learning_rate=0.01, momentum=0.9),  # Ottimizzatore SGD
+        optimizer=SGD(learning_rate=0.011, momentum = 0.9),  # Ottimizzatore SGD
         loss=BinaryCrossentropy(),         # Loss Binary Cross Entropy
         metrics=['accuracy'] ,              # Metrica Accuracy
 
@@ -55,17 +62,17 @@ def train_neural_network(train_path, test_path):
     # Definizione del callback per early stopping
     early_stopping = EarlyStopping(
         monitor='val_loss',  # Monitoriamo la perdita sulla validazione
-        patience=100,  # Numero di epoche da aspettare senza miglioramenti
+        patience=15,  # Numero di epoche da aspettare senza miglioramenti
         restore_best_weights=True  # Ripristina i pesi migliori al termine
     )
 
     # Addestramento del modello
-    model.fit(
-        X_train, y_train,
-        epochs=500,
+    hist = model.fit(
+        X_train_encoded, y_train,
+        epochs=100,
         batch_size=16,
-        validation_data=(X_val, y_val),
-        #callbacks=[early_stopping]
+        validation_data=(X_val_encoded, y_val),
+        callbacks=[early_stopping]
     )
 
     # Valutazione del modello
