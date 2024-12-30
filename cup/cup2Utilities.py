4f50eaad9dc4a6f22af09a6cbe8cdecf1829bb52
Gabibbocomrade <Tank2001@hotmail.it>
2024-12-30 20:34:26 +0100

    Utilities di backup

--- cup/cup2Utilities.py
+++ cup/cup2Utilities.py
@@ -1,50 +1,35 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-class DatasetProcessor:
+class DatasetProcessor2:
     """
     Classe per il caricamento e la pre-elaborazione dei dataset di training e test.
     """
 
     def load_dataset(self, train_path, test_path):
-        """
-        Carica i dataset di training e test da file CSV.
 
-        Args:
-            train_path (str): Percorso del file di training.
-            test_path (str): Percorso del file di test.
-
-        Returns:
-            tuple: (DataFrame di training, DataFrame di test)
-        """
         df_train = pd.read_csv(train_path, sep=r"\s+", header=None)
         df_test = pd.read_csv(test_path, sep=r"\s+", header=None)
         return df_train, df_test
 
     def preprocess_data(self, df_train, df_test):
-        """
-        Pre-elabora i dataset di training e test.
 
-        Args:
-            df_train (DataFrame): Dataset di training.
-            df_test (DataFrame): Dataset di test.
-
-        Returns:
-            tuple: (X_train, y_train, X_test, y_test) pre-elaborati come array numpy.
-        """
         # Separazione delle feature e del target
-        Y_train = df_train.iloc[:, -3:]
-        X_train = df_train.iloc[:, :-3]
+        Y = df_train.iloc[:, -3:]
+        X = df_train.iloc[:, 2:-3]
         
+        X_ts = df_test.iloc[:,2:-3]
 
         # Conversione in array numpy
-        X_train = X_train.select_dtypes(include=[np.number]).to_numpy()
-        y_train = Y_train.to_numpy()
+        #X_train = X_train.select_dtypes(include=[np.number]).to_numpy()
+        #y_train = Y_train.to_numpy()
 
+        X_train,X_test,Y_train,Y_test = train_test_split(X, Y, test_size= 0.15, random_state=42)
 
-        return X_train, y_train
+        return X_train, Y_train, X_test, Y_test
 
     def normalize_data(self, X_train, X_test):
         scaler = StandardScaler()
