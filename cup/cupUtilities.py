8740ed8fe014a0ddda11fc19b3937eecc8d2bdd5
Gabibbocomrade <Tank2001@hotmail.it>
2025-01-02 19:52:21 +0100

    Aggiunta funzione

--- cup/cupUtilities.py
+++ cup/cupUtilities.py
@@ -27,3 +27,21 @@ class DatasetProcessor:
         file = os.path.join(self.root_dir, "datasets", "ml-cup", "ML-CUP24-TS.csv")
         test = np.loadtxt(file, delimiter=',', usecols=range(1, 13), dtype=np.float64)
         return test
+
+    def write_blind_results(y_pred):
+
+        assert len(y_pred) == 472, "Not enough data were predicted! 472 predictions expected!"
+
+        file = os.path.join(ROOT_DIR, BLIND_TEST_FILENAME)
+        with open(file, "w") as f:
+            print("# Tank \t Leo", file=f)
+            print("# Martana", file=f)
+            print("# ML-CUP24", file=f)
+            print("# 02/01/2024", file=f)
+
+            pred_id = 1
+            for p in y_pred:
+                print("{},{},{}".format(pred_id, p[0], p[1]), file=f)
+                pred_id += 1
+
+        f.close()
